21933d30e42270abd14a04bc56f1ff1c804fb64e
George Maina <83180647+GeorgeKMaina@users.noreply.github.com>
2024-06-28 13:07:06 +0300

    Update loanpredicitionstream.py

--- loanpredicitionstream.py
+++ loanpredicitionstream.py
@@ -10,9 +10,12 @@ import pandas as pd
 import pickle
 import streamlit as st
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
 
 # Load the trained model
-loaded_model = pickle.load(open("trained_model.sav", 'rb'))
+loaded_model = pickle.load(open(r"C:\Users\gmaina\Downloads\trained_model.sav", 'rb'))
+product_encoder = pickle.load(open(r"C:\Users\gmaina\Downloads\product_encoder.sav", 'rb'))
+investor_encoder = pickle.load(open(r"C:\Users\gmaina\Downloads\investor_encoder.sav", 'rb'))
 
 #creating a function for prediction
 def loanprediction(input_data):
@@ -46,12 +49,49 @@ def main():
     #giving a title
     st.title("Credit Predicition Web App")
     
+    #select boxes for product
+    product=('airtime', 
+             'data_bundles', 
+             'financial_services',
+             'movies',
+             'retail',
+             'tv',
+             'utility_bill'
+             )
+    
+    investor=('InvestorId_1',
+              'InvestorId_2',
+              'InvestorId_3',
+              'NA'
+              )
+    
+    # Initialize and fit the LabelEncoder for products and investors
+    product_encoder = LabelEncoder()
+    product_encoder.fit(product)
+    investor_encoder= LabelEncoder()
+    investor_encoder.fit(investor)
+    
+    
+    
+    
     #getting the input data from the user
     Customer_ID = st.text_input("What is the customer ID")
-    Value = st.text_input("The monetary value of the transaction")
-    Amount = st.text_input("Value of the transaction including any charges")
-    Product_id = st.text_input("What item is being bought")
-    Investor_id = st.text_input("What is the loan issuer or network owner")
+    Value = st.slider("The monetary value of the transaction",min_value=0, max_value=10000)
+    if Value == 0:
+        Amount = st.slider("Value of the transaction including any charges", min_value=0, max_value=10000)
+    else:
+        Amount = st.slider("Value of the transaction including any charges", min_value=Value, max_value=int(Value + (0.2 * Value)))
+        
+    Product_id = st.selectbox("What item is being bought",product)
+    Investor_id = st.selectbox("What is the loan issuer or network owner",investor)
+    
+    # Encode categorical variables
+    Product_id_encoded = product_encoder.transform([Product_id])[0]
+    Investor_id_encoded = investor_encoder.transform([Investor_id])[0]
+
+    # Convert input data to appropriate types
+    input_data = [int(Customer_ID), float(Value), float(Amount), Product_id_encoded, Investor_id_encoded]
+
     
     #code for predicition
     
@@ -59,17 +99,9 @@ def main():
     
     # creating button for prediction
     if st.button("Loan Prediction Results"):
-        predicta = loanprediction([Customer_ID,Value,Amount,Product_id,Investor_id])
+        predicta = loanprediction(input_data)
         
     st.success(predicta)
         
 if __name__ == '__main__':
     main()
-    
-    
-    
-    
-    
-    
-    
-    
